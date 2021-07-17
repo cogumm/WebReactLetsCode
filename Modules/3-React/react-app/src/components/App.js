327712e7a39d56b617716fd532e4da890396ba6a
Gabriel F. Vilar <gabriel@cogumm.net>
2021-07-17 12:51:48 -0300

    Aula 05 - Props

---
+++ Modules/3-React/react-app/src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <p>Estou vivo!</p>
+        <p>Fragment.</p>
+        <p>{new Date().toLocaleDateString("pt-BR")}</p>
+      </>
+    );
+  }
+}
+
+export default App;
--- Modules/3-React/react-app/src/index.js
+++ Modules/3-React/react-app/src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import reportWebVitals from "./reportWebVitals";
+
+// import App from "./components/App";
+import App2 from "./components/App2";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <App2 title="Titulo">Texto</App2>
+    <App2 title="Titulo2">Texto2</App2>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 reportWebVitals();
