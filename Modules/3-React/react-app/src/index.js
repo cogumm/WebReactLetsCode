6580e236a2f240bccbc4d3fce58099e9cf02ac77
Gabriel F. Vilar <gabriel@cogumm.net>
2021-07-18 18:17:42 -0300

    Aula 10 - Componentes Aninhados

--- Modules/3-React/react-app/src/index.js
+++ Modules/3-React/react-app/src/index.js
@@ -7,7 +7,8 @@ import reportWebVitals from "./reportWebVitals";
 // import App3 from "./components/App3";
 // import App4 from "./components/App4";
 // import App5 from "./components/App5";
-import Lista from "./components/Lista";
+// import Lista from "./components/Lista";
+import { Item, Lista2 } from "./components/Lista2";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -17,7 +18,16 @@ ReactDOM.render(
     {/* <App3 /> */}
     {/* <App4 /> */}
     {/* <App5 /> */}
-    <Lista />
+    {/* <Lista /> */}
+    <Lista2>
+      Texto
+      <Item key={999} id={999} completo={false}>
+        999
+      </Item>
+      <Item key={998} id={998} completo={true}>
+        998
+      </Item>
+    </Lista2>
   </React.StrictMode>,
   document.getElementById("root")
 );
